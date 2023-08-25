d1a69c66f1793a5e1663d554c87d1e160f1c43bc
lin-ci <1375917042@qq.com>
2023-08-25 23:52:24 +0800

    实现点赞和获取喜欢视频接口

--- favoriteService/internal/favoriteImp/favoriteImp.go
+++ favoriteService/internal/favoriteImp/favoriteImp.go
@@ -2,7 +2,9 @@ package favoriteImp
 
 import (
 	"context"
+	"favoriteService/models"
 	"favoriteService/services/favoriteService"
+	"userService/rpcClients"
 )
 
 // 请在此完成服务的业务逻辑
@@ -10,9 +12,94 @@ type FavoriteService struct {
 }
 
 func (*FavoriteService) FavoriteAction(ctx context.Context, request *favoriteService.DouyinFavoriteActionRequest, response *favoriteService.DouyinFavoriteActionResponse) error {
+	videoId := request.VideoId
+	actionType := request.ActionType
+	if request.Token == "" {
+		//response todo
+		return nil
+	}
+	//从 token 中解析出登录用户的 id
+	userId, err := rpcClients.GetIdByToken(request.Token)
+	if err != nil {
+		// response.StatusCode = -1
+		// response.StatusMsg = "登录失效，请重新登录"
+		// response.User = &userService.User{}
+		// return nil
+	}
+	video, err := models.NewVideoDaoInstance().FindVideoById(videoId)
+	if err != nil {
+		return err
+	}
+	if actionType == 1 {
+		//视频获赞数+1
+		models.NewVideoDaoInstance().AddFavoriteCount(videoId, 1)
+		//用户喜欢数+1
+		models.NewUserDaoInstance().AddFavoriteCount(userId, 1)
+		//插入一条点赞记录
+		models.NewFavDaoInstance().AddFavRec(userId, videoId)
+		//视频作者被点赞数+1
+		models.NewUserDaoInstance().AddTotalFavorited(video.UserId, 1)
+	} else if actionType == 2 {
+		models.NewVideoDaoInstance().AddFavoriteCount(videoId, -1)
+		models.NewUserDaoInstance().AddFavoriteCount(userId, -1)
+		models.NewFavDaoInstance().DelFavRec(userId, videoId)
+		models.NewUserDaoInstance().AddTotalFavorited(video.UserId, -1)
+	}
 	return nil
 }
 
 func (*FavoriteService) FavoriteList(ctx context.Context, request *favoriteService.DouyinFavoriteListRequest, response *favoriteService.DouyinFavoriteListResponse) error {
+	if request.Token == "" {
+		//response todo
+		return nil
+	}
+	//从 token 中解析出登录用户的 id
+	userId, err := rpcClients.GetIdByToken(request.Token)
+	if err != nil {
+		return err
+	}
+	videoIdList, err := models.NewFavDaoInstance().GetFavVideoIdList(userId)
+	if err != nil {
+		return err
+	}
+	videoList := make([]*favoriteService.Video, 0)
+	for _, id := range videoIdList {
+		video, err := models.NewVideoDaoInstance().FindVideoById(id)
+		if err != nil {
+			return err
+		}
+		authorId := video.UserId
+		author, err := models.NewUserDaoInstance().GetUserById(authorId)
+		if err != nil {
+			return err
+		}
+		videoList = append(videoList, &favoriteService.Video{
+			Id:      video.VideoId,
+			PlayUrl: video.PlayUrl,
+			Author: &favoriteService.User{
+				Id:              author.UserId,
+				Name:            author.Name,
+				FollowCount:     author.FollowingCount,
+				FollowerCount:   author.FollowerCount,
+				IsFollow:        false, //unknow
+				Avatar:          author.Avatar,
+				BackgroundImage: author.BackgroundImage,
+				Signature:       author.Signature,
+				TotalFavorited:  author.TotalFavorited,
+				WorkCount:       author.WorkCount,
+				FavoriteCount:   author.WorkCount,
+			},
+			CoverUrl:      video.CoverUrl,
+			FavoriteCount: video.FavoriteCount,
+			CommentCount:  video.CommentCount,
+			IsFavorite:    true,
+			Title:         video.Title,
+		})
+	}
+	response = &favoriteService.DouyinFavoriteListResponse{
+		StatusCode: 0,
+		StatusMsg:  "获取点赞视频列表成功",
+		VideoList:  videoList,
+	}
 	return nil
 }
--- favoriteService/models/favoriteModel.go
+++ favoriteService/models/favoriteModel.go
@@ -1,5 +1,66 @@
 package models
 
+import (
+	"sync"
+)
+
 // 请在此完成对 favarite 服务所用表的定义
 type Favorite struct {
+	UserId  int64 `gorm:"primaryKey"`
+	VideoId int64 `gorm:"primaryKey"`
+}
+type FavoriteDao struct {
+}
+
+var favDao *FavoriteDao
+var favOnce sync.Once //单例模式，只生成一个VideoDao实例，提高性能
+
+func NewFavDaoInstance() *FavoriteDao {
+	favOnce.Do(
+		func() {
+			favDao = &FavoriteDao{}
+		})
+	return favDao
+}
+
+// 查询是否存在user点赞video的记录
+func (f *FavoriteDao) IsFavorite(userId int64, VideoId int64) (bool, error) {
+	ret := false
+	var rec Favorite
+	result := SqlSession.Where("User_id = ? and Video_id = ?", userId, VideoId).Find(rec)
+	err := result.Error
+	if err != nil {
+		return ret, err
+	}
+	ret = true
+	return ret, err
+}
+
+// 根据UserId获取关注视频Id列表
+func (f *FavoriteDao) GetFavVideoIdList(userId int64) ([]int64, error) {
+	var rec []int64
+	result := SqlSession.Where("User_id = ? ", userId).Find(&rec)
+	err := result.Error
+	if err != nil {
+		return rec, err
+	}
+	return rec, err
+}
+
+func (f *FavoriteDao) AddFavRec(userId int64, videoId int64) error {
+	fav := Favorite{
+		UserId:  userId,
+		VideoId: videoId,
+	}
+	result := SqlSession.Create(&fav)
+	return result.Error
+}
+
+func (f *FavoriteDao) DelFavRec(userId int64, videoId int64) error {
+	fav := Favorite{
+		UserId:  userId,
+		VideoId: videoId,
+	}
+	result := SqlSession.Delete(&fav)
+	return result.Error
 }
